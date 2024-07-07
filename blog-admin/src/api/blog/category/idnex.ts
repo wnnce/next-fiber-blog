0a15bd4d2e44078508e9bd3617672e9e1ecd8547
CrushCola <ranchangxin@gmail.com>
2024-07-08 00:15:18 +0800

    添加博客分类接口

---
+++ blog-admin/src/api/blog/category/idnex.ts
@@ -0,0 +1,32 @@
+import { sendDelete, sendGet, sendPost, sendPut } from '@/api/request'
+import type { Category, CategoryForm } from '@/api/blog/category/types'
+
+export const categoryApi = {
+  /**
+   * 获取分类树型结构数据
+   */
+  listTree: () => {
+    return sendGet<Category[]>('/system/category/manage/list');
+  },
+  /**
+   * 保存分类
+   * @param form 分类参数
+   */
+  saveCategory: (form: CategoryForm) => {
+    return sendPost<null>('/system/category', form)
+  },
+  /**
+   * 更新分类
+   * @param form
+   */
+  updateCategory: (form: CategoryForm) => {
+    return sendPut<null>('/system/category', form)
+  },
+  /**
+   * 删除分类
+   * @param id 分类Id
+   */
+  deleteCategory: (id: number) => {
+    return sendDelete<null>(`/system/category/${id}`)
+  }
+}
---
+++ blog-admin/src/api/blog/category/types.ts
@@ -0,0 +1,40 @@
+// 博客分类
+export interface Category {
+  // 分类Id
+  categoryId: number;
+  // 分类名称
+  categoryName: string;
+  // 类型描述
+  description: string;
+  // 封面
+  coverUrl: string;
+  // 查看次数
+  viewNum: number;
+  // 上级Id
+  parentId: number;
+  // 是否热门分类
+  isHot: boolean;
+  // 分类是否置顶
+  isTop: boolean;
+  // 排序
+  sort: number;
+  // 状态
+  status: number;
+  // 创建时间
+  createTime: string;
+  // 子分类
+  children: Category[]
+}
+
+// 分类表单
+export interface CategoryForm {
+  categoryId?: number;
+  categoryName: string;
+  description?: string;
+  coverUrl: string;
+  parentId?: number;
+  isHot: boolean;
+  isTop: boolean;
+  sort: number;
+  status: number;
+}
