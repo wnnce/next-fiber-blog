ce534e851640e52f58f6795ef9be827475a9d290
CrushCola <ranchangxin@gmail.com>
2024-08-05 21:08:12 +0800

    Sql参数缓存区不再使用map缓存重复参数的序号

--- go-fiber-server/pkg/sql-build/parameter.go
+++ go-fiber-server/pkg/sql-build/parameter.go
@@ -3,6 +3,7 @@ package sqlbuild
 // StatementParameterBuffer SQL命令参数缓存接口
 type StatementParameterBuffer interface {
 	Args() []any
+	// 由于map不能使用切片做为key 暂时弃用
 	addParameter(param any) int
 }
 
@@ -19,15 +20,16 @@ func (self *PostgresStatementParameterBuffer) addParameter(param any) int {
 	if self.args == nil {
 		self.args = make([]any, 0)
 	}
-	if self.argsIndexMap == nil {
+	// map不能使用切片做为key 其次同一条sql中重复参数的比例不会太高 故而先不使用map存储参数索引
+	/*if self.argsIndexMap == nil {
 		self.argsIndexMap = make(map[any]int)
 	}
 	index, ok := self.argsIndexMap[param]
 	if ok {
 		return index
-	}
+	}*/
 	self.args = append(self.args, param)
-	index = len(self.args)
-	self.argsIndexMap[param] = index
-	return index
+	/*index = len(self.args)
+	self.argsIndexMap[param] = index*/
+	return len(self.args)
 }
