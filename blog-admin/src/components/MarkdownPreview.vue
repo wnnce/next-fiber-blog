08e77b414838c563a1cbe6c5730ddaa28c0ce8b4
CrushCola <ranchangxin@gmail.com>
2024-08-08 22:09:41 +0800

    添加markdown预览组件

---
+++ blog-admin/src/components/MarkdownPreview.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Vditor from 'vditor/dist/method.min'
+import { onMounted } from 'vue'
+
+interface Props {
+  markdown: string
+}
+
+const props = defineProps<Props>();
+
+const templateId: string = (Math.random() * 100).toFixed(0);
+
+onMounted(() => {
+  Vditor.preview(<HTMLDivElement>document.getElementById(templateId), props.markdown, {
+    mode: 'dark',
+    hljs: {
+      lineNumber: true,
+      style: 'native'
+    },
+  })
+})
+
+</script>
+
+<template>
+  <div class="preview-container" :id="templateId">
+
+  </div>
+</template>
+
+<style scoped lang="scss">
+.preview-container {
+  color: var(--text-color) !important;
+}
+</style>
