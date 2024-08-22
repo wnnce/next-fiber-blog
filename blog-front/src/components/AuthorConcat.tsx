30eb35aab20e2458782d758d4ad9ba18482ca96d
CrushCola <ranchangxin@gmail.com>
2024-08-22 14:00:21 +0800

    联系方式和作者信息卡片使用后端数据

---
+++ blog-front/src/components/AuthorConcat.tsx
@@ -0,0 +1,78 @@
+import '@/styles/components/author-link.scss';
+import React from 'react'
+import DynamicCard from '@/components/DynamicCard'
+import RichImage from '@/components/RichImage'
+import { Concat } from '@/lib/types'
+import useSiteConfiguration from '@/hooks/site-configuration'
+import { listConcat } from '@/lib/api'
+
+/**
+ * 作者联系方式组件
+ * @param concats 联系方式列表
+ * @constructor
+ */
+const ConcatList: React.FC<{
+  concats: Concat[]
+}> = ({ concats }): React.ReactNode => {
+  const firstMainConcat = concats.find(item => item.isMain);
+  const list = !firstMainConcat ? concats : concats.filter(item => item.concatId != firstMainConcat.concatId);
+  return (
+    <div className="mt-4">
+      { firstMainConcat && (
+        <a href={firstMainConcat.targetUrl} target="_blank" className="main-link" title={firstMainConcat.name}>
+          <i className="mr-1" dangerouslySetInnerHTML={{ __html: firstMainConcat.iconSvg }} />
+          {firstMainConcat.name}
+        </a>
+      )}
+      <ul className="list-none flex flex-wrap justify-center gap-row-2 mt-4 text-xl author-links-ul">
+        { list.map(concat => (
+          <li className="w-20% text-center" key={concat.concatId}>
+            <a href={concat.targetUrl} title={concat.name} target="_blank" className="inline-block"
+               dangerouslySetInnerHTML={{ __html: concat.iconSvg }} >
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+/**
+ * 博客作者信息个链接组件
+ * @constructor
+ */
+const AuthorConcat: React.FC = async () => {
+  const [avatar, title, summary] = await useSiteConfiguration().queryConfigs('avatar', 'title', 'summary');
+  const { data: concats } = await listConcat();
+  return (
+    <DynamicCard padding="1.5rem">
+      <section>
+        <div className="flex justify-center">
+          <RichImage className="author-avatar" src={avatar.value.toString()} width={112} height={112} fill
+                     thumbnail
+                     radius="50%"
+                     alt="author-avatar"
+          />
+        </div>
+        <h1 className="text-center font-bold text-2xl line-height-relaxed text-wrap mt-2">{title.value}</h1>
+        <p className="text-center text-sm text-wrap info-text">{summary.value}</p>
+        <ul className="list-none flex justify-around mt-4">
+          <li className="text-center">
+            <span className="text-sm font-mono info-text">POSTS</span><br />
+            <a href="/page/1" className="text-lg"><strong>5</strong></a>
+          </li>
+          <li className="text-center">
+            <span className="text-sm font-mono info-text">CATEGORIES</span><br />
+            <a href="/categorys" className="text-lg"><strong>5</strong></a>
+          </li>
+          <li className="text-center">
+            <span className="text-sm font-mono info-text">TAGS</span><br />
+            <a href="/tags" className="text-lg"><strong>5</strong></a>
+          </li>
+        </ul>
+        <ConcatList concats={concats} />
+      </section>
+    </DynamicCard>
+  )
+}
+export default AuthorConcat;
--- blog-front/src/lib/api.ts
+++ blog-front/src/lib/api.ts
@@ -1,5 +1,6 @@
 import { request } from '@/lib/request'
-import { Category, Result, Tag } from '@/lib/types'
+import { Category, Concat, Notice, Result, Tag } from '@/lib/types'
+import { SiteConfigurationItem } from '@/hooks/site-configuration'
 
 /**
  * 获取博客分类列表
@@ -13,4 +14,18 @@ export const listCategory = (): Promise<Result<Category[]>> => {
  */
 export const listTag = (): Promise<Result<Tag[]>> => {
   return request<Tag[]>('/open/tag/list', 'GET');
+}
+
+/**
+ * 配置博客站点配置
+ */
+export const querySiteConfiguration = (): Promise<Result<Record<string, SiteConfigurationItem>>> => {
+  return request<Record<string, SiteConfigurationItem>>('/open/site/configuration', 'GET')
+}
+
+/**
+ * 获取联系方式列表
+ */
+export const listConcat = (): Promise<Result<Concat[]>> => {
+  return request<Concat[]>('/open/concat/list', 'GET');
 }
