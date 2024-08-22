9b750e5ed6e8adceb87b75c6d2d7522c3c4a017b
CrushCola <ranchangxin@gmail.com>
2024-08-22 14:01:26 +0800

    底部Footer使用后端数据

--- blog-front/src/layouts/Footer.tsx
+++ blog-front/src/layouts/Footer.tsx
@@ -1,8 +1,10 @@
 import '@/styles/layouts/footer.scss';
 import React from 'react';
 import Image from 'next/image'
+import useSiteConfiguration from '@/hooks/site-configuration'
 
-const Footer: React.FC = (): React.ReactNode => {
+const Footer: React.FC = async () => {
+  const [ title, powered, icp ] = await useSiteConfiguration().queryConfigs('title', 'powered', 'icp')
   return (
     <footer className="footer">
       <div className="dynamic-container sm:flex justify-between">
@@ -13,16 +15,15 @@ const Footer: React.FC = (): React.ReactNode => {
           <div className="mt-4 text-xs line-height-loose">
             <p className="flex items-center">
               <i className="i-tabler-copyright inline-block mr-1"></i >
-              2022-2024 By&nbsp;<strong>CrushCola</strong>
+              2022-2024 By&nbsp;<strong>{ title.value }</strong>
             </p>
-            <p>
-              Powered By <a target="_blank" href="https://vercel.com">Vercel</a> | <a target="_blank" href="https://nextjs.org">NextJS</a> | <a target="_blank" href="https://go.dev">Golang</a> | <a target="_blank" href="https://gofiber.org">GoFiber</a>
-            </p>
+            { powered.value && <p>Powered By <span dangerouslySetInnerHTML={{ __html: powered.value }} /></p> }
             <p>苟利国家生死以 | 岂因福祸避趋之</p>
-            <p><a href="https://baidu.com" target="_blank">渝ICP备20001411号</a></p>
+            {icp.value && <p><a href="https://baidu.com" target="_blank">{icp.value}</a></p>}
           </div>
         </div>
-        <div className="footer-right-icons flex gap-col-4 justify-end flex-wrap items-center text-7 max-w-md mt-6 sm:mt-0">
+        <div
+          className="footer-right-icons flex gap-col-4 justify-end flex-wrap items-center text-7 max-w-md mt-6 sm:mt-0">
           <a className="inline-block i-tabler-creative-commons" href="https://baidu.com" target="_blank" />
           <a className="inline-block i-tabler-brand-github" href="https://github.com/wnnce" target="_blank" />
           <a className="inline-block i-tabler-brand-golang" href="https://go.dev/" target="_blank" />
