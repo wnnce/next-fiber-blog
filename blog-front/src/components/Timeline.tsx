6fe1a8170aaf7060e29dd3db050436ecb83e6811
CrushCola <ranchangxin@gmail.com>
2024-08-26 23:55:11 +0800

    添加时间轴组件

---
+++ blog-front/src/components/Timeline.tsx
@@ -0,0 +1,29 @@
+import '@/styles/components/timeline.scss'
+import React from 'react'
+
+export const Timeline: React.FC<{
+  children: React.ReactNode
+}> = ({ children }) => {
+  return (
+    <ul className="list-none timeline">
+      { children }
+    </ul>
+  )
+}
+
+export const TimeLineItem: React.FC<{
+  time: Date | string
+  children: React.ReactNode
+}> = ({ time, children }) => {
+  return (
+    <li className="timeline-item">
+      <div className="timeline-item-time desc-text line-height-loose flex items-center py-2">
+        <i className="inline-block i-tabler:clock mr-1.5"></i>
+        <time dateTime={time.toString()} className="text-xs ">{ time.toString() }</time>
+      </div>
+      <div className="p-4 timeline-item-body">
+        { children }
+      </div>
+    </li>
+  )
+}
