b30f9d17420d9f7c79e55aff9100617c1c1b1492
CrushCola <ranchangxin@gmail.com>
2024-08-27 16:24:29 +0800

    动态列表添加照片墙模式

--- blog-front/src/app/(index)/topic/page/[page]/page.tsx
+++ blog-front/src/app/(index)/topic/page/[page]/page.tsx
@@ -6,6 +6,7 @@ import SimpleMarkdown from '@/components/SimpleMarkdown'
 import StaticCard from '@/components/StaticCard'
 import { Timeline, TimeLineItem } from '@/components/Timeline'
 import RichImage from '@/components/RichImage'
+import { CommonLike } from '@/components/ClictComponents'
 
 const TopicPage: React.FC<{
   params: {
@@ -17,7 +18,7 @@ const TopicPage: React.FC<{
   if (!numberPage || isNaN(numberPage) || numberPage <= 0 ) {
     throw new Error('动态参数错误')
   }
-  const { data: topicPage } = await pageTopic({ page: numberPage, size: 10 })
+  const { data: topicPage } = await pageTopic({ page: numberPage, size: 10 });
   return (
     <>
       <StaticCard padding="1.5rem" title="TOPICS" icon="i-tabler:world" multiple={40}>
@@ -29,17 +30,23 @@ const TopicPage: React.FC<{
             <TimeLineItem key={topic.topicId} time={topic.createTime}>
               <SimpleMarkdown className="topic-list-li-content" markdown={topic.content} />
               { (topic.imageUrls && topic.imageUrls.length > 0) && (
-                <div className="flex flex-wrap gap-1 mt-4">
-                  { topic.imageUrls.map(image => (
-                    <RichImage src={image} fill thumbnail preview radius={4} key={image}/>
-                  ))}
-                </div>
+                topic.mode === 1 ? (
+                  <div className="flex flex-wrap gap-1 mt-4">
+                    {topic.imageUrls.map(image => (
+                      <RichImage src={image} width={100} height={100} fill thumbnail preview radius={4} key={image} />
+                    ))}
+                  </div>
+                ) : (
+                  <div className="topic-flow-photo mt-4">
+                    { topic.imageUrls.map(image => (
+                      <RichImage src={image} key={image} width={0} height={0} imageClassName="w-full h-auto" thumbnail preview />
+                    )) }
+                  </div>
+                )
               )}
               <ul className="flex gap-col-4 mt-4 desc-text relative">
                 <li className="flex items-center">
-                  <i className="inline-block i-tabler:heart" />
-                  {/*<i className="inline-block text-red-5 i-tabler:heart-filled" />*/}
-                  <span className="font-mono ml-1">{ topic.voteUp }</span>
+                  <CommonLike count={topic.voteUp} entityKey={topic.topicId} type="topic" />
                 </li>
                 <li className="flex items-center">
                   <i className="inline-block i-tabler:message-chatbot" />
